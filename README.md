400064e5292c121c68f476423925f5116787f1d0
Mikael Johansson <mik.json@gmail.com>
2024-04-12 12:36:06 +0200

    First lines of code

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # argocd-extra-app-info-exporter
 
-`argocd-extra-app-info-exporter` - TODO: Add tagline here
+`argocd-extra-app-info-exporter` - Exports that one missing metric from ArgoCD.
+
+This exporter exports a `argocd_extra_app_info` metric which looks like the already exported `argocd_app_info` metric but with the `targetRevision` field as label.
+
+This exporter will probably be around until https://github.com/argoproj/argo-cd/pull/15143 merges!
 
 ## Install
 
 1. `make build`
-
-## Usage
-
-Example:
-```
-TODO: Add example here
-```
--- cmd/argocd-extra-app-info-exporter/main.go
+++ cmd/argocd-extra-app-info-exporter/main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
+	argo "github.com/argoproj/argo-cd/v2/pkg/client/clientset/versioned"
 	"github.com/mikejoh/argocd-extra-app-info-exporter/internal/buildinfo"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
-type argocd-extra-app-info-exporterOptions struct {
-	version bool
+type exporterOptions struct {
+	version    bool
+	kubeconfig *string
 }
 
 func main() {
-	var argocd-extra-app-info-exporterOpts argocd-extra-app-info-exporterOptions
-	flag.BoolVar(&argocd-extra-app-info-exporterOpts.version, "version", false, "Print the version number.")
+	var exporterOpts exporterOptions
+	flag.BoolVar(&exporterOpts.version, "version", false, "Print the version number.")
 	flag.Parse()
 
-	if argocd-extra-app-info-exporterOpts.version {
+	if exporterOpts.version {
 		fmt.Println(buildinfo.Get())
 		os.Exit(0)
+	}
+
+	if home := homedir.HomeDir(); home != "" {
+		exporterOpts.kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		exporterOpts.kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *exporterOpts.kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientset, err := argo.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	apps, err := clientset.ArgoprojV1alpha1().Applications("").List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, app := range apps.Items {
+		fmt.Println(app.Name)
 	}
 }
